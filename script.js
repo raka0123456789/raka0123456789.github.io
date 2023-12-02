2d5b5470aad8b87c8b987b59a8e1e0e0aa27dcfd
raka0123456789 <152395079+raka0123456789@users.noreply.github.com>
2023-12-02 18:13:31 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+function education(){
+  document.getElementById("edu").innerHTML = "Kalikuning Elementary School. 2011-2017";
+document.getElementById("edu2").innerHTML = "Takhassus Al Qur'an Junior High School. 2017-2020";
+  document.getElementById("edu3").innerHTML = "Takhassus Al Qur'an Vocational High School. 2020-2023";
+  document.getElementById("edu4").innerHTML = "Al Qur'an University of Science. 2023 - now";
+}
+
+function quote(){
+  document.getElementById("Quote").innerHTML = "Don't forget your God.! because your God never forgets you.";
+}
